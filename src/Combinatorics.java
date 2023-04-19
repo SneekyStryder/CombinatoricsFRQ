5285ecd40e073f32e1db315dc1dcc1fdf59e55eb
Imran Fazilov <ifazilov3584@bths.edu>
2023-04-19 11:45:04 -0400

    Done

--- src/Combinatorics.java
+++ src/Combinatorics.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class Combinatorics {
+public class Combinatorics
+
+{
+
+    /** Precondition: n is between 1 and 12, inclusive.
+
+     * Returns the factorial of n, as described in part (a).
+
+     */
+
+    public static int factorial(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        else if (n > 1) {
+            return n * factorial(n - 1);
+        }
+        return 0;
+    }
+
+
+
+    /** Precondition: n and r are between 1 and 12, inclusive.
+
+     * Determines the number of ways r items can be selected
+
+     * from n choices and prints the result, as described in part (b).
+
+     */
+
+    public static void numCombinations(int n, int r) {
+        if (r > n) {
+            System.out.println("There are 0 ways of choosing " + r + " items from " + n + " choices.");
+        }
+        else if (r == n) {
+            System.out.println("There is 1 way of choosing " + r + " items from " + n + " choices.");
+        }
+        else {
+            int combins = factorial(n) / (factorial(r) * factorial(n - r));
+            System.out.println("There are " + combins + " ways of choosing " + r + " items from " + n + " choices.");
+        }
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        Combinatorics.numCombinations(2, 4);
+        Combinatorics.numCombinations(5, 3);
+        Combinatorics.numCombinations(5, 5);
+        Combinatorics.numCombinations(11, 6);
+    }
 }
